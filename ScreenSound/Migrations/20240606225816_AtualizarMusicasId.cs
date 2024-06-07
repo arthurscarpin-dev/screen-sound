cd2f5429776367290a9be3a0a5eb2da7e10600c8
Arthur Scarpin <carvalhoscarpin@gmail.com>
2024-06-06 21:34:59 -0300

    adicionando entity framework

---
+++ ScreenSound/Migrations/20240606225816_AtualizarMusicasId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizarMusicasId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Musica SET ArtistaId = 1 WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE Musica SET ArtistaId = 2 WHERE Id = 2");
+            migrationBuilder.Sql("UPDATE Musica SET ArtistaId = 3 WHERE Id = 3");
+            migrationBuilder.Sql("UPDATE Musica SET ArtistaId = 4 WHERE Id = 4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musica");
+        }
+    }
+}
